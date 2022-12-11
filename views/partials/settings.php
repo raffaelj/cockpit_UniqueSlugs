d03e76f8a8801f5a905e770b013342f492aca749
Raffael <github@raffaeljesche.de>
2022-12-11 10:17:51 +0100

    cleanup

--- views/partials/settings.php
+++ views/partials/settings.php
@@ -1,11 +1,11 @@
 
 <div>
-    <div class="uk-panel uk-panel-space uk-panel-box uk-panel-card">
-        <img src="@url('assets:app/media/icons/settings.svg')" width="50" height="50" alt="@lang('UniqueSlugs')" />
+    <div class="uk-panel uk-panel-space uk-panel-box uk-panel-card uk-panel-card-hover">
+        <img src="@url('assets:app/media/icons/settings.svg')" width="50" height="50" alt="" />
 
         <div class="uk-text-truncate uk-margin">
-            @lang('UniqueSlugs')
+            UniqueSlugs
         </div>
-        <a class="uk-position-cover" aria-label="@lang('UniqueSlugs')" href="@route('/uniqueslugs/settings')"></a>
+        <a class="uk-position-cover" aria-label="UniqueSlugs" href="@route('/uniqueslugs/settings')"></a>
     </div>
 </div>
